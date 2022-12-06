f982c1f0d55c8cb1d7fdc43adc43c2cc4b67ed51
smmmfrd <smmmfrd@gmail.com>
2022-12-06 14:12:48 -0800

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
           </Route>
         </Routes>
       </GameContextProvider>
-
+      <Footer />
     </BrowserRouter>
   );
 }
@@ -36,4 +36,13 @@ function Navbar() {
       <Outlet />
     </>
   );
+}
+
+function Footer() {
+  return (
+    <footer className="fixed bottom-0 w-full p-2 bg-slate-500 text-neutral-50 flex md:flex-row flex-col gap-4 md:gap-16 justify-around items-center">
+      <p>No Association w/ Hasbro</p>
+      <p>By <a className="underline" href="https://github.com/smmmfrd" target='_blank' rel="noopener noreferrer">@smmmfrd</a> &#169; 2021</p>
+    </footer>
+  )
 }
