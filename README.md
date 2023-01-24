f50aa1d3a6f20b463f30631405edc622697464ec
smmmfrd <smmmfrd@gmail.com>
2023-01-24 02:38:28 -0800

    Icon Attribute and Marker Styling Fix

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ When I first saw React's structure I knew I would love it, as it mirrors how I w
 After learning all I could about React I decided to redo an earlier [Odin Project assignment](https://www.theodinproject.com/lessons/node-path-javascript-battleship) and make a game in React.
 
 [Live link](https://velvety-moxie-6a1467.netlify.app/)
+
+Ship Icon from [remixicon](https://remixicon.com/)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className="fixed bottom-0 w-full p-2 bg-slate-500 text-neutral-50 flex flex-row gap-4 justify-around items-center">
+      <footer className="fixed bottom-0 w-full p-2 bg-slate-500 text-neutral-50 z-20 flex flex-row gap-4 justify-around items-center">
         <p>No Association w/ Hasbro</p>
         <p>By <a className="underline" href="https://github.com/smmmfrd" target='_blank' rel="noopener noreferrer">@smmmfrd</a> &#169; 2021</p>
       </footer>
